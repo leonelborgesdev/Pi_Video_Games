db2906412694f992bc80130fed7f9f76775299c4
leonelborgesdev <leonel03t@gmail.com>
2024-02-18 02:39:23 -0400

    dotenv

--- api/src/database.ts
+++ api/src/database.ts
@@ -1,9 +1,11 @@
+require("dotenv").config();
 import { Pool } from "pg";
+const { DB_USER, DB_PASSWORD, DB_HOST} = process.env;
 
 export const pool=new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'uajms',
+    user: DB_USER,
+    host: DB_HOST,
+    password: DB_PASSWORD,
     database: 'videogames',
     port: 5432
 })
