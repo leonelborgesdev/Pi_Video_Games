b07e78fa6b18f77afc35e2e6bdd7d2715daa5639
leonelborgesdev <leonel03t@gmail.com>
2024-02-17 20:11:19 -0400

    button landing

--- client/src/Landign/Landing.jsx
+++ client/src/Landign/Landing.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import VideoGames from "../assets/VideoGames.png";
 import "./Landing.css";
+import { Link } from "react-router-dom";
 
 export const Landing = () => {
   return (
@@ -11,6 +12,7 @@ export const Landing = () => {
       <div className="container_body_landing">
         <div className="container_detail_landing">
           <h2>Detalle de video games</h2>
+          <Link to="/videogames">Entrar</Link>
         </div>
         <div className="container_image_landing">
           <img src={VideoGames} alt="" />
