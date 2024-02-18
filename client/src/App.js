d5a8e6617f1b5316492fae438c52a25db6a67e8a
leonelborgesdev <leonel03t@gmail.com>
2024-02-17 20:06:20 -0400

    react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import { Landing } from "./Landign/Landing.jsx";
+import { VideoGames } from "./VideoGames/VideoGames.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Landing />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/videogames" element={<VideoGames />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
