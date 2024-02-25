255a80999cf8e677a43eae3b3af475613635daf3
leonelborgesdev <leonel03t@gmail.com>
2024-02-24 23:14:02 -0400

    route detallevideogames

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { DetalleVideoGames } from "./Components/DetalleVideoGames/DetalleVideoGames.jsx";
 import { Landing } from "./Components/Landign/Landing.jsx";
 import { VideoGames } from "./Components/VideoGames/VideoGames.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/videogames" element={<VideoGames />} />
+          <Route path="/videogames/:id" element={<DetalleVideoGames />} />
         </Routes>
       </BrowserRouter>
     </div>
